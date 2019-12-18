ecf1ba03517ebc9090572920dbdcec61da5f7409
CNife <CNife@vip.qq.com>
2019-12-18 15:51:52 +0800

    添加基本的 token 定义

--- javacc.ps1
+++ javacc.ps1
@@ -2,5 +2,6 @@ java -classpath "C:\Code\javacc-6.0\bin\lib\javacc.jar" `
     javacc `
         -JDK_VERSION="1.8" `
         -OUTPUT_DIRECTORY="./src/generated" `
+        -IGNORE_CASE=true `
         -DEBUG_PARSER=true `
         AndroidTotemSqlParser.jj
